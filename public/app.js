be28604aa3fdb5468da5e4973938b47aba91ba2c
Jonathan Lee <jlee2384@uwo.ca>
2016-03-12 06:23:45 -0500

    gets json response from food2fork

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 (function(){
+  /*
   var app = angular.module('nhacks',['ui.router', 'ngRoute']);
 
   app.config(function ($routeProvider, $locationProvider, $stateProvider, $urlRouterProvider) {
@@ -10,7 +11,21 @@
 
     $locationProvider.html5Mode(true);
   });
+*/
 
+  var app = angular.module('nhacks',['ui.router']);
+
+  app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    $stateProvider
+      .state('home', {
+        url:'/',
+        templateUrl: '/index.html',
+        controller: "FoodSearchCtrl"
+      });
+
+      $urlRouterProvider.otherwise('home');
+      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  });
   app.filter('capitalize', function() {
     return function(input) {
       return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
@@ -18,10 +33,41 @@
   });
 
 //////////////////////////////////////////////////////////////////////////////////////
-  app.controller('IndexCtrl', function($rootScope, $scope, $routeParams, $http, trip){
-    $scope.trip = trip;
+ app.controller('IndexCtrl', function($rootScope, $scope, $routeParams, $http){
+    //$scope.trip = trip;
 
 
+  });
+
+  app.controller('FoodSearchCtrl', function($scope, $http){
+
+    $scope.results = null;
+    $scope.foodSearch = function() {
+      var url = 'http://cors.io?u=http://www.food2fork.com/api/search?key=529cd164050b80734aff7a59a2f7a0a3&q=salmon';
+
+/*
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+      }).done(function(response) {
+        console.log(response);
+        $scope.results = response;
+      });
+      */
+
+      $http({
+        url: url,
+        method: 'GET'
+      }).then(function(response) {
+        //console.log(response.status);
+        //console.log(res.data);
+        $scope.results = response;
+      }, function(response) {
+        console.log(response.body);
+      }); 
+
+    };
   });
 
 
